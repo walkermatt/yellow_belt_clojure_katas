2a99d4e576bd4cdb64b64d36fb4d0ae5d8508867
Matt Walker <walkermatt@longwayaround.org.uk>
2014-02-18 08:54:57 +0000

    Faster loop/recur version of fib...
    ... that came to me before I dropped off to sleep last night

--- src/clojure_katas/fib.clj
+++ src/clojure_katas/fib.clj
@@ -1,12 +1,25 @@
 (ns clojure-katas.fib
   (:use [midje.sweet]))
 
-(defn fib[n]
-  (if (< n 2) n
-      (+ (fib (dec n)) (fib (- n 2)))))
+;; Simple recursive version which consumes a lot of stack for "large" values
+; (defn fib[n]
+;   (if (< n 2) n
+;       (+ (fib (dec n)) (fib (- n 2)))))
 
-; F0 	 F1 	 F2 	 F3 	 F4 	 F5 	 F6 	 F7 	 F8
-; 0  	 1  	 1  	 2  	 3  	 5  	 8  	 13 	 21
+;; loop/recur version which can handle calculating much larger values and
+;; is faster. Builds a vector of fibonacci numbers which it uses to calculate
+;; successive values
+(defn fib[n]
+  (loop [nums []]
+    (let [c (count nums)
+          nums (conj nums (if (< c 2)
+                            c
+                            (reduce + (take-last 2 nums))))]
+      (if (= c n)
+        ; We're done, return the last value
+        (last nums)
+        ; Loop back passing the vector of values we've calculated so far
+        (recur nums)))))
 
 (facts "Calculate the fibonacci sequence
 f(0) = 0, f(1) = 1
@@ -14,4 +27,7 @@ f(n) = f(n-1) + f(n-2)"
          (fib 0) => 0
          (fib 1) => 1
          (fib 3) => 2
-         (fib 10) => 55)
+         (fib 7) => 13
+         (fib 10) => 55
+         (fib 16) => 987
+         (fib 20) => 6765)
